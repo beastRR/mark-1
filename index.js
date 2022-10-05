6b752cf0a82b2a01a21d9ec151e02d51631b1f62
beastRR <89104224+beastRR@users.noreply.github.com>
2022-10-05 16:46:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+var readlineSync = require("readline-sync");
+var userName = readlineSync.question("What's your Name? ");
+console.log("Welcome  "  + userName
+ +  " Do you know Rakesh ?");
+ score=0;
+
+ console.log("welcome to the Quiz")
+
+ function play(question,answer){
+   var userAnswer =readlineSync.question(question)
+   if(userAnswer === answer){
+     console.log("You are right")
+     score = score+1;
+
+   }
+   else{
+     console.log("You are Wrong");
+     score = score-1;
+   }
+ console.log("Your currentscore is:" ,score);
+ }
+
+ var questions  = [
+   {question:"Where do he Iive ?",answer:"ranchi"},
+
+ {question:"where did he  go to summer camp? ", answer:"goa"},
+ {question:" Is he an Android or iPhone person?",answer:"android"},
+ {question:"What is his favorite place?",answer:"ranchi"},
+ 
+ {question:"What is his favorite series?",answer:"breaking bad"},
+ {question:"Do he have any addictions?", answer:"no"}];
+
+
+for(var i=0; i<questions.length; i++){
+  var currentQuestion = questions[i];
+  play(currentQuestion.question,currentQuestion.answer);
+  
+}
+if(score >3){
+    console.log("You know him very well")
+  }
+  else{
+
+
+    console.log("As your currentscore is very low you do not know him very well")
+      }
